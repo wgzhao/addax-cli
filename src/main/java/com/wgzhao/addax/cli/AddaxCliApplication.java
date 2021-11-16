4cd9e48d1a2fe0c436c054964b1c876090eaa271
wgzhao <wgzhao@gmail.com>
2021-11-16 15:40:52 +0800

    Fix WebApplicationContext exception

--- src/main/java/com/wgzhao/addax/cli/AddaxCliApplication.java
+++ src/main/java/com/wgzhao/addax/cli/AddaxCliApplication.java
@@ -2,15 +2,13 @@ package com.wgzhao.addax.cli;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.RestController;
 
 @EnableAsync
 @ComponentScan(basePackages = {"com.*"})
-@MapperScan(basePackages = "com.wgzhao.addax.cli.mapper")
+@MapperScan(basePackages = "com.wgzhao.addax.admin.mapper")
 @SpringBootApplication
 public class AddaxCliApplication {
     public static void main(String[] args) {
